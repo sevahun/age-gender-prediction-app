f0ec4d0683b9ce6217aaa978f4a550785cd51346
Sevara Amirullaeva <seva99.sa@gmail.com>
2023-12-07 12:52:17 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ PATH = Path(__file__).parent
 
 ### HELPER FUNCTIONS ###
 def get_data_specs(data_name):
-    datas = {"agedb": {"age_num": 100}, "afad": {"age_num": 57}, "cacd": {"age_num": 49}}
+    datas = {"agedb": {"age_num": 100}, "afad": {"age_num": 57}}
     age_num = datas[data_name]["age_num"]
     labels_txt = json.load(open("age_labels.txt"))
     labels = torch.tensor(labels_txt[data_name])
@@ -25,8 +25,7 @@ def download_chpts(data):
 
     else:
         chpt_ids = {"agedb": "1IFU1TAih8gZqSJsCntJzBYW3WxZMBpB9",
-                    "afad": "1dzbYe9VW8RbE-2Zv2X4xTUihVVv7mXQA",
-                    "cacd": "1rHqxPV_Qjj1kJ5_XMzYfBlhWZPCcuwW8"}
+                    "afad": "1dzbYe9VW8RbE-2Zv2X4xTUihVVv7mXQA"}
 
         id = chpt_ids[data]
         prefix = "https://drive.google.com/uc?/export=download&id="
@@ -49,21 +48,19 @@ def predict(image, model, age_labels, data):
             mean=[0.485, 0.456, 0.406],
             std=[0.229, 0.224, 0.225]
         )])
+    
     img = image
     batch_t = torch.unsqueeze(transform(img), 0)
     model.eval()
-    if data == "cacd":
-        pred_age = model(batch_t)
-        pred_age = torch.sum(pred_age * age_labels, dim=1)
-        pred_gen = None
-        pred_gen_lbl = None
+    
+    pred_age, pred_gen = model(batch_t)
+    _, pred_gen_lbl = torch.max(pred_gen, 1)
+    pred_gen_lbl = pred_gen_lbl.item()
+    
+    if pred_gen_lbl == 0:
+        pred_gen = "Male"
     else:
-        pred_age, pred_gen = model(batch_t)
-        _, pred_gen_lbl = torch.max(pred_gen, 1)
-        pred_gen_lbl = pred_gen_lbl.item()
-        if pred_gen_lbl == 0:
-            pred_gen = "Male"
-        else:
-            pred_gen = "Female"
-        pred_age = torch.sum(pred_age * age_labels, dim=1)
+        pred_gen = "Female"
+        
+    pred_age = torch.sum(pred_age * age_labels, dim=1)
     return pred_age, pred_gen, pred_gen_lbl
